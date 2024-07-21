621e322192726278eeee7e586b7d561766713bc8
SafiAchraf <a_safi@estin.dz>
2024-07-21 22:40:20 +0100

    io.reader

--- learn.go
+++ learn.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
+func main() {
+	r := strings.NewReader("Hello, Reader!")
 
-func main(){
-	fmt.Print(10)
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
-
