e1153d9ce15a6c06eace24ced16a358eb8de85e2
SafiAchraf <a_safi@estin.dz>
2024-08-03 23:25:49 +0100

    adding upd

---
+++ UDPserver.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", ":8080")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("UDP server listening on port 8080")
+
+	// Buffer to hold incoming data
+	buffer := make([]byte, 1024)
+	newaddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3434")
+
+	for {
+		// Read from the connection
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err)
+			continue
+		}
+		
+		fmt.Printf("Received %s from %s\n", string(buffer[:n]), addr)
+		_, err = conn.WriteToUDP([]byte("Hello from UDP server"), newaddr)
+
+		if err != nil {
+			fmt.Println("Error sending data:", err)
+		}
+	
+	}
+}
